5f10b4a3fe9e9cfe4a39606d0bed211c89b17574
Nek0tikGrey <56598472+Nek0tikGrey@users.noreply.github.com>
2021-01-26 14:37:18 +0200

    Add some models

---
+++ Models/FileModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTwinCoreProject.Models
+{
+    public class FileModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Extensions { get; set; }
+        public Type TypeTo { get; set; }
+        public Guid Guid { get; set; }
+
+        public enum Type
+        {
+            Avatar,
+            Note
+        }
+    }
+
+}
