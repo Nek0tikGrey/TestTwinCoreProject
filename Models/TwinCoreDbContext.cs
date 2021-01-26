d3eb3221dd6332ba02c0c5b54abaa5ddc3501a35
Nek0tikGrey <56598472+Nek0tikGrey@users.noreply.github.com>
2021-01-26 15:42:08 +0200

    Change id type to Guid

--- Models/TwinCoreDbContext.cs
+++ Models/TwinCoreDbContext.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace TestTwinCoreProject.Models
 {
-    public class TwinCoreDbContext : IdentityDbContext<Account>
+    public class TwinCoreDbContext : IdentityDbContext<Account,ApplicationRole,Guid>
     {
         public DbSet<FileModel> Files { get; set; }
         public TwinCoreDbContext(DbContextOptions<TwinCoreDbContext> options):base(options)
         {
             Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
