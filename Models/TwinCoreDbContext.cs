2d43057110e6daf73064cc26dd72c1a5146367d7
Nek0tikGrey <56598472+Nek0tikGrey@users.noreply.github.com>
2021-01-26 15:25:58 +0200

    Adding File controller

--- Models/TwinCoreDbContext.cs
+++ Models/TwinCoreDbContext.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TestTwinCoreProject.Models
 {
-    public class TwinCoreDbContext:IdentityDbContext<Account>
+    public class TwinCoreDbContext : IdentityDbContext<Account>
     {
-
+        public DbSet<FileModel> Files { get; set; }
         public TwinCoreDbContext(DbContextOptions<TwinCoreDbContext> options):base(options)
         {
             Database.EnsureCreated();
