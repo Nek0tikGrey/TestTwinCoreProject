2b8cd1954009a50f0a43a832457c22a7bc3b64d1
Nek0tikGrey <56598472+Nek0tikGrey@users.noreply.github.com>
2021-02-25 12:06:48 +0200

    Prepeare

---
+++ ViewModels/UserAccountViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTwinCoreProject.Models;
+
+namespace TestTwinCoreProject.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IList<FileModel> Avatars { get; set; }
+
+        UserAccountViewModel()
+        {
+            Avatars = new List<FileModel>();
+        }
+    }
+}
