b962a288da9d595251d3bc99b4c7d4174c9b03cb
Nek0tikGrey <56598472+Nek0tikGrey@users.noreply.github.com>
2021-03-11 15:53:17 +0200

    Show image in note tag helper

---
+++ TagHelpers/FileTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Linq;
+using TestTwinCoreProject.Models;
+
+namespace TestTwinCoreProject.TagHelpers
+{
+    public class FileTagHelper: TagHelper
+    {
+        public Guid Guid { get; set; }
+        private readonly TwinCoreDbContext context;
+        public FileTagHelper(TwinCoreDbContext context)
+        {
+            this.context = context;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "post-content-img");
+            output.TagMode = TagMode.StartTagAndEndTag;
+            string content = "";
+            foreach (var t in this.context.Files.Where(p => p.Guid == Guid))
+            {
+                content+=$"<img src=\"{t.Path}\" />";
+            }
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
